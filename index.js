fa8ac1391e7ca56ef8350ecf150ede1df7db35eb
bappy <bappy3a@gmail.com>
2024-07-30 14:08:39 +0600

    file

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const os = require('os');
+const fs = require('fs');
 
-console.log(os.cpus());
+fs.writeFile('myFile2.text', 'Hello Bappy', () => {});
+fs.appendFile('myFile2.text', ' Hello Bappy now update', () => {});
+
+fs.readFile('myFile.text', (e, data) => {
+    console.log(data.toString());
+});
+// fs.unlinkSync('myFile2.text');
+// fs.rename('myFile3.text', 'myFile5.text', () => {});
